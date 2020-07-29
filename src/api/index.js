081e4eedeac556a1999260be77bcf5cdc6f1a48e
duchong <long5262@qq.com>
2020-07-29 10:36:53 +0800

    接口链接调整，注册成功页面调整

--- src/api/index.js
+++ src/api/index.js
@@ -1,19 +1,19 @@
 const method = {
     user:{
-        login: '/web/v1/login',
+        login: '/login',
     },
     merchant:{
-        submit: '/h5/v1/merchant/merchantApply',
+        submit: '/merchant/merchantApply',
     },
     trade:{
         createOrder:'com.zanclick.createOrder',
         queryState:'com.zanclick.query.orderState',
-        create:'/h5/v1/create/auth/prePay',
+        create:'/create/auth/prePay',
         queryContract:'con.zanclick.zcpay.contract.getOrderDetail',
         saveContract:'con.zanclick.zcpay.contract.saveNameAndCertNo',
     },
     upload:{
-        getUploadToken:'upload/getToken'
+        getUploadToken:'/upload/getToken'
     },
 }
 export default method;
